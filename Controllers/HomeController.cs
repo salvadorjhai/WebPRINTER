4540bb017f80a6cdb1a9f1b13e5f574e8ce260e2
salvadorjhai <jhai_salvador@yahoo.com>
2024-11-13 12:27:02 +0800

    sample rdlc included

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -4,7 +4,9 @@ using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.Drawing.Printing;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Web;
 using System.Web.Mvc;
 
@@ -34,7 +36,20 @@ namespace WebPRINTER.Controllers
 
         public ActionResult Print()
         {
-            var res = new PDFPrinter().Print("D:\\_WORK\\MEMBERSHIP ID CARD - SINGLE.pdf");
+            var selectedprinter = Request.QueryString["printer"];
+
+            LocalReport lr = new LocalReport ();
+            lr.ReportPath = Path.Combine(Server.MapPath("/REPORT/"), "Report1.rdlc");
+            lr.DataSources.Clear();
+
+            // lr.DataSources.Add(new ReportDataSource("DataSet1", models));
+            
+            // print by local report (rdlc)
+            var res = new PDFPrinter().Print(lr, selectedprinter);
+
+            // print by file
+            // var res = new PDFPrinter().Print("D:\\_WORK\\MEMBERSHIP ID CARD - SINGLE.pdf", selectedprinter);
+
             return Content("OK");
         }
     }
